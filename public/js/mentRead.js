8b8071f70f22551d8b1d544e50bab1d65e071eed
Fanathing <mmybonnie@gamil.com>
2025-09-09 14:24:05 +0900

    Feat: mentDelete.js 구현 및 유저데이터가 일치하지 않으면 삭제버튼과 수정 버튼이 생성되지 않게 수정

--- public/js/mentRead.js
+++ public/js/mentRead.js
@@ -4,11 +4,14 @@ const mentRead = () => {
     const replyDate = JSON.parse(localStorage.getItem("replyData")) || [];
     const request = new URLSearchParams(location.search);
     const key = Number(request.get("uniquekey"));
+    const loginData = JSON.parse(localStorage.getItem('loginUserData'));
+    const loginUserId = loginData.userId;
     
     for (let i = 0; i < replyDate.length; i++) {
         if (key === replyDate[i].parentId) {
             const commentLi = document.createElement("li");
-            commentLi.setAttribute("class", "commentlist")
+            commentLi.setAttribute("class", "commentlist");
+            commentLi.setAttribute("data-index", i);
             commentLi.setAttribute("data-key", replyDate[i].replyId);
 
             const commentId = document.createElement("p");
@@ -16,19 +19,26 @@ const mentRead = () => {
 
             const commentCt = document.createElement("p");
             commentCt.textContent = `${replyDate[i].mentContent}`;
+            
+ 
+            if (loginUserId === replyDate[i].mentUserId) {
 
-            const commentDB = document.createElement("button");
-            commentDB.setAttribute("class", "delete-btn");
-            commentDB.setAttribute("onclick" , "deleteBtn()");
-            commentDB.textContent = `삭제`;
+                const commentDB = document.createElement("button");
+                commentDB.setAttribute("class", "delete-btn");
+                commentDB.textContent = `삭제`;
 
-            const commentUP = document.createElement("button");
-            commentUP.setAttribute("class", "update-btn");
-            commentUP.setAttribute("onclick", "updateBtn()");
-            commentUP.textContent = `수정`;
+                const commentUP = document.createElement("button");
+                commentUP.setAttribute("class", "update-btn");
+                commentUP.setAttribute("onclick", "updateBtn()");
+                commentUP.setAttribute("type", "button");
+                commentUP.textContent = `수정`;
+                mentAreaList.appendChild(commentLi);
+                commentLi.append(commentId,commentCt,commentDB,commentUP);
+            }else{
+                mentAreaList.appendChild(commentLi);
+                commentLi.append(commentId,commentCt);
+            }
 
-            mentAreaList.appendChild(commentLi);
-            commentLi.append(commentId,commentCt,commentDB,commentUP);
             }
     }
 }
